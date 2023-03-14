5efb7d06d55ed30ad3a1ec68392b6f49d61290a3
Moritz Hoffmeyer <moritzhoffmeyer@icloud.com>
2023-03-14 14:46:06 +0100

    Add SearchQuery function (still bugged)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
-const searchBar = document.querySelector('[data-js="search-bar"]');
+const searchBarInput = document.querySelector(".search-bar__input");
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
@@ -12,16 +12,22 @@ const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
 // Anzahl max. Seiten, wird nicht verändert
-const maxPage = 42;
+let maxPage = 42;
 
 //Aktuelle Seitenzahl, verändert sich durch die fetchCharacters() Funktion und passt sich der aktuellen Seitenzahl an.
 let page = 1;
-const searchQuery = "";
 
-async function fetchCharacters(pageCount = 1) {
+let query = "";
+
+searchBarInput.addEventListener("input", (event) => {
+  query = event.target.value;
+  fetchCharacters(page, query);
+});
+
+async function fetchCharacters(pageCount = 1, searchQuery = "") {
   //Fetch Rick&Morty Datensatz mit entsprechender Seitenzahl (= pageCount)
   const fetchJsonData = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${pageCount}`
+    `https://rickandmortyapi.com/api/character?page=${pageCount}&name=${searchQuery}`
   );
 
   //Transformiere Datensatz in JavaScript-verwertbares Objekt
@@ -30,10 +36,12 @@ async function fetchCharacters(pageCount = 1) {
   //Store Array of Characters in Variable 'characters'
   let { results: characters } = data;
 
-  //reset global variable to current pageCount (Parameter der Funktion, der unsere Seitenzahl der API widesspiegelt)
+  let { info } = data;
+  maxPage = info.pages;
   page = pageCount;
+  pagination.innerHTML = `${pageCount} / ${maxPage}`;
 
-  if (pageCount === 42) {
+  if (pageCount === maxPage) {
     nextButton.disabled = true;
   } else if (pageCount === 1) {
     prevButton.disabled = true;
@@ -49,7 +57,7 @@ async function fetchCharacters(pageCount = 1) {
   cardContainer.innerHTML = "";
 
   //Iterate over each character Object and define the parameters, needed for our CreateCards Function
-  characters.forEach((character, index) => {
+  characters.forEach((character) => {
     let name = character.name;
     let imgSrc = character.image;
     let status = character.status;
@@ -63,11 +71,11 @@ async function fetchCharacters(pageCount = 1) {
 
 //Set NextButton Eventlistener
 nextButton.addEventListener("click", () => {
-  fetchCharacters(++page);
+  fetchCharacters(++page, query);
 });
 
 prevButton.addEventListener("click", () => {
-  fetchCharacters(--page);
+  fetchCharacters(--page, query);
 });
 
 fetchCharacters();
